4fee53505093c2b74d981cbae4f13dd8a50951f3
mrvauxs <mrvauxs@gmail.com>
2023-04-24 08:27:32 +0000

    sub-categories in navigation

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -1,6 +1,8 @@
 <script>
-	import { page } from '$app/stores';
 	import { drawerStore } from '@skeletonlabs/skeleton';
+	import NavigationOption from './NavigationOption.svelte';
+	import { page } from '$app/stores';
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
 
 	function drawerClose() {
 		drawerStore.close();
@@ -13,6 +15,48 @@
 			icon: 'home'
 		},
 		{
+			label: 'Openable',
+			icon: 'home',
+			pages: [
+				{
+					label: 'Openable 1',
+					href: '/openable/1',
+					icon: 'home'
+				},
+				{
+					label: 'Openable 2',
+					href: '/openable/2',
+					icon: 'home'
+				},
+				{
+					label: 'Openable 3',
+					href: '/openable/3',
+					icon: 'home'
+				}
+			]
+		},
+		{
+			label: 'Openable 2',
+			icon: 'home',
+			pages: [
+				{
+					label: 'Openable 4',
+					href: '/openable/4',
+					icon: 'home'
+				},
+				{
+					label: 'Openable 5',
+					href: '/openable/5',
+					icon: 'home'
+				},
+				{
+					label: 'Openable 6',
+					href: '/openable/6',
+					icon: 'home'
+				}
+			]
+		},
+		{
 			label: 'About',
 			href: '/about',
 			icon: 'about'
@@ -23,15 +67,24 @@
 <nav class="list-nav p-2">
 	<ul>
 		{#each pages as link}
-			<li>
-				<a
-					on:click={drawerClose}
-					class:!variant-ghost-surface={link.href === $page.url.pathname}
-					href={link.href}
-				>
-					{link.label}
-				</a>
-			</li>
+			{#if link.pages}
+				<Accordion>
+					<AccordionItem open={link.pages.find((x) => x.href === $page.url.pathname)}>
+						<svelte:fragment slot="summary">
+							{link.label}
+						</svelte:fragment>
+						<svelte:fragment slot="content">
+							{#each link.pages as sublink}
+								<NavigationOption link={sublink} openFunction={drawerClose} />
+							{/each}
+						</svelte:fragment>
+					</AccordionItem>
+				</Accordion>
+			{:else}
+				<li>
+					<NavigationOption {link} openFunction={drawerClose} />
+				</li>
+			{/if}
 		{/each}
 	</ul>
 </nav>
---
+++ src/lib/Navigation/NavigationOption.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+	export let link = {};
+	export let openFunction;
+	export let selected = false;
+</script>
+
+<a
+	on:click={openFunction}
+	class:!variant-ghost-surface={selected || link.href === $page.url.pathname}
+	href={link.href}
+>
+	{link.label}
+</a>
