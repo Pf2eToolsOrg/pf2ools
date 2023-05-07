d7a33dc5c04655e7576478d7aca92bb7229609f5
MrVauxs <mrvauxs@gmail.com>
2023-05-07 13:45:23 +0200

    fix top level await

--- src/routes/ancestries/+page.js
+++ src/routes/ancestries/+page.js
@@ -3,17 +3,16 @@ const modules = import.meta.glob([
     '!$data/ancestries/versatile-heritages.json'
 ]);
 
-const ancestries = [];
-for (const path in modules) {
-    await modules[path]().then((mod) => {
-        ancestries.push(...mod.ancestry)
-    });
-}
-
 // lookup https://vitejs.dev/guide/features.html#json
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
+    const ancestries = [];
+    for (const path in modules) {
+        await modules[path]().then((mod) => {
+            ancestries.push(...mod.ancestry)
+        });
+    }
     return {
         ancestries
     };
