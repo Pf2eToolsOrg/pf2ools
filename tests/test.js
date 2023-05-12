5ffdbee2cf64f55ea89ccc3efaaa70da3919c2af
MrVauxs <mrvauxs@gmail.com>
2023-05-12 18:02:36 +0200

    fix tests

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,4 @@
-import { expect, test } from '@playwright/test';
-
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Hello Skeleton' })).toBeVisible();
-});
+import { test } from '@playwright/test';
 
 test('license page has expected the open game license', async ({ page }) => {
 	await page.goto('/');
