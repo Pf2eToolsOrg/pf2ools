9a082fe2dcaac939e7cbff270490ab8765c4997b
MrVauxs <MrVauxs@users.noreply.github.com>
2023-05-12 16:05:07 +0000

    Deploying to gh-pages from @ Pf2eToolsOrg/pf2ools@5ffdbee2cf64f55ea89ccc3efaaa70da3919c2af 🚀

--- _app/immutable/chunks/stores.ddf72dae.js
+++
@@ -1,1 +0,0 @@
-import"./index.10f9c6f3.js";import{s as r}from"./singletons.5899963b.js";const e=()=>{const s=r;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},a={subscribe(s){return e().page.subscribe(s)}},o={subscribe(s){return e().navigating.subscribe(s)}};export{o as n,a as p};
