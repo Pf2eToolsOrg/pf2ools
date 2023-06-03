fe483020fad19e3769792e819aba1e6e41ba0412
MrVauxs <MrVauxs@users.noreply.github.com>
2023-06-03 18:06:06 +0000

    Deploying to gh-pages from @ Pf2eToolsOrg/pf2ools@be41de2876bf79bc420319e8f4d97136f0acffe7 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), c = [
-  a + "/_app/immutable/entry/app.94430ee7.js",
-  a + "/_app/immutable/assets/0.ffbc6345.css",
-  a + "/_app/immutable/nodes/0.277bfd86.js",
-  a + "/_app/immutable/nodes/1.55b2b5a9.js",
+  a + "/_app/immutable/entry/app.97199054.js",
+  a + "/_app/immutable/assets/0.f4834ddc.css",
+  a + "/_app/immutable/nodes/0.a28bcdb7.js",
+  a + "/_app/immutable/nodes/1.d131c6ea.js",
   a + "/_app/immutable/nodes/2.848cd800.js",
   a + "/_app/immutable/assets/3.dbbd372c.css",
-  a + "/_app/immutable/nodes/3.2002b463.js",
+  a + "/_app/immutable/nodes/3.41c2937d.js",
   a + "/_app/immutable/nodes/4.f1b8c6fb.js",
   a + "/_app/immutable/assets/5.e1fa6f69.css",
   a + "/_app/immutable/nodes/5.5b63bbf5.js",
@@ -16,10 +16,10 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), c
   a + "/_app/immutable/chunks/index.67b0cfc4.js",
   a + "/_app/immutable/chunks/index.a78c7a44.js",
   a + "/_app/immutable/chunks/preload-helper.41c905a7.js",
-  a + "/_app/immutable/chunks/singletons.c45086e4.js",
+  a + "/_app/immutable/chunks/singletons.3d7b8dad.js",
+  a + "/_app/immutable/chunks/stores.09bd49ef.js",
   a + "/_app/immutable/chunks/stores.15ee31a9.js",
-  a + "/_app/immutable/chunks/stores.a000ef7c.js",
-  a + "/_app/immutable/entry/start.3241d8d9.js",
+  a + "/_app/immutable/entry/start.2b35b132.js",
   a + "/_app/immutable/chunks/ancestry-anadi.1dfa6c74.js",
   a + "/_app/immutable/chunks/ancestry-android.22dcb6be.js",
   a + "/_app/immutable/chunks/ancestry-automaton.3efcee38.js",
@@ -165,7 +165,7 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), c
   a + "/icons/safari-pinned-tab.svg",
   a + "/images/AoN.webp",
   a + "/manifest.webmanifest"
-], r = [], u = "1685814423161", f = `cache-${u}`, l = [
+], r = [], u = "1685815546586", f = `cache-${u}`, l = [
   ...c,
   // the app itself
   ...r,
